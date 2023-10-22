7fc99aa4517eabdf4de464426854fbcf40800dce
Pedro Ribeiro <pedroribeiro212@gmail.com>
2023-10-22 08:37:18 -0400

    Commit inicial no repositório local

---
+++ src/classe/Data.java
@@ -0,0 +1,8 @@
+package classe;
+
+public class Data {
+
+	String dia;
+	String mes;
+	String ano;
+}
---
+++ src/classe/ProdutoTeste.java
@@ -0,0 +1,12 @@
+package classe;
+
+public class ProdutoTeste {
+
+	public static void main(String[] args) {
+		
+		Produto produto1 = new Produto();
+		var produto2 = new Produto();
+
+	}
+
+}
